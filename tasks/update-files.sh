21c5216d51bc761ca9b17dff81c66ac757c92b49
Nic Aitch <nic@nicinabox.com>
2016-01-02 14:07:24 -0600

    Support rules

--- tasks/update-files.sh
+++ tasks/update-files.sh
@@ -2,3 +2,12 @@
 
 git checkout master -- docs LICENSE CHANGELOG.md CONTRIBUTING.md
 git cat-file blob master:README.md > index.md
+
+git co master -- src/rules
+git checkout src
+git reset src
+
+mv src docs
+rm -rf docs/src/rules/**/__tests__
+rm -rf docs/src/rules/**/*.js
+rm -rf src/
